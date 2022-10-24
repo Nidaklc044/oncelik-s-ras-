f511ebbcfb002f063313eb2cbe2c954c8fb9c143
Nidaklc044 <116462585+Nidaklc044@users.noreply.github.com>
2022-10-24 10:56:29 +0300

    girilen işlemlerde öncelik
    Visual Studio ile c#

---
+++ odevuygulama1/odevuygulama1/Program.cs
@@ -0,0 +1,8 @@
+﻿
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine(48/6/4);//2 soldan sağa işlem yapıldı.
+Console.WriteLine(24/3*2);//16 soldan sağa işlem yapıldı.
+Console.WriteLine(Math.Pow(2,2)+5-1+4);//12 soldan sağa işlem yapıldı.
+Console.WriteLine((7+4)*2-1+8/2);//25 soldan sağa işlem yapıldı.
+Console.WriteLine((5-1)*2-1+Math.Pow(7,2)/2);//31.5 
+
